78bdb5a03c013f171f08cfb210b967bdb81eb629
Artemproyd <aptemproyd@list.ru>
2025-04-29 18:16:21 +0700

    sss111

--- lab2/container_script.sh
+++ lab2/container_script.sh
@@ -31,14 +31,17 @@ do
         flock -x 200
         
         # Ищем первое свободное имя файла
-        for i in $(seq -f "%03g" 1 999)
-        do
-            if [ ! -f "/shared/$i" ]; then
-                echo "$CONTAINER_ID:$COUNTER" > "/shared/$i"
-                echo "[$CONTAINER_ID] Создал файл $i (операция $COUNTER)"
-                break
-            fi
-        done
+        LAST_FILE=$(ls -1 /shared/[0-9][0-9][0-9] 2>/dev/null | sort -n | tail -n 1)
+        if [ -z "$LAST_FILE" ]; then
+            # Если файлов нет, начинаем с 001
+            i="001"
+        else
+            # Берем следующий номер после последнего существующего
+            i=$(printf "%03d" $((10#${LAST_FILE##*/} + 1)))
+        fi
+        
+        echo "$CONTAINER_ID:$COUNTER" > "/shared/$i"
+        echo "[$CONTAINER_ID] Создал файл $i (операция $COUNTER)"
     ) 200>/shared/.lock
     
     # Пауза ровно 1 секунда между операциями
